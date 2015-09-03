8592e3706bcf22d4e94d4cd780c9fa449822c42f
Lucas Dourado <lucasldourado@gmail.com>
2015-09-03 16:43:37 -0300

    cadastro da votacao da uev

--- app/models/CandidatoModel.php
+++ app/models/CandidatoModel.php
@@ -54,4 +54,59 @@ class CandidatoModel {
 		}
 	}
 
+	public function cadastraVotos($dados) {
+
+		if(!is_object($dados) || empty($dados))
+			return false;
+
+		$flagAtiva = 1;
+		$flagInativa = 0;
+		$flagNull = null;
+		
+		$this->db->beginTransaction();
+
+		foreach ($dados as $idCargo => $dadosVotos) {
+			foreach ($dadosVotos as $numCandidato => $qtdVotos) {
+				$sql = 'INSERT INTO votacao (id_cargo, numero_candidato,'
+					.'voto_branco, voto_nulo, qtd_votos) values ('
+					.':id_cargo, :numero_candidato, :voto_branco,'
+					.':voto_nulo, :qtd_votos)';
+
+				$query = $this->db->prepare($sql);
+
+				$query->bindParam("id_cargo", $idCargo, \PDO::PARAM_INT);
+				$query->bindParam("qtd_votos", $qtdVotos, \PDO::PARAM_INT);
+
+				if($numCandidato == 'branco') {
+					$query->bindParam("numero_candidato", $flagNull, \PDO::PARAM_NULL);
+					$query->bindParam("voto_branco", $flagAtiva, \PDO::PARAM_INT);
+					$query->bindParam("voto_nulo", $flagInativa, \PDO::PARAM_INT);
+				}
+
+				if($numCandidato == 'nulo') {
+					$query->bindParam("numero_candidato", $flagNull, \PDO::PARAM_NULL);
+					$query->bindParam("voto_branco", $flagInativa, \PDO::PARAM_INT);
+					$query->bindParam("voto_nulo", $flagAtiva, \PDO::PARAM_INT);
+				}
+
+				if($numCandidato != 'branco' && $numCandidato != 'nulo') {
+					$query->bindParam("numero_candidato", $numCandidato, \PDO::PARAM_INT);
+					$query->bindParam("voto_branco", $flagInativa, \PDO::PARAM_INT);
+					$query->bindParam("voto_nulo", $flagInativa, \PDO::PARAM_INT);
+				}
+
+				$resultado = $query->execute();
+
+				if(!$resultado) {
+					$this->db->rollback();
+					return false;
+				}
+			}
+		}
+
+		$this->db->commit();
+
+		return true;
+	}
+
 }
